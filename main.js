ece6ac901e2934e4811e09ca1d24101e7578b202
Kwony7 <126170052+Kwony7@users.noreply.github.com>
2023-03-25 16:59:45 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var arr = ["nothing","nothing", "nothing", "JACKPOT", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing"];
+
+
+
+
+/*function randomNum(){
+    rand = Math.floor(Math.random() * 20);
+}
+*/
+function clik(){
+    var rand = Math.floor(Math.random() * 20);
+    alert(arr[rand]);
+    console.log(rand);
+}
+/*
+setInterval(randomNum(), 500);
+*/
